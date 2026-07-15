0334c8248ccae340ccccc6e0bb3714695db7d501
prashant-1012 <prashant2009kr@gmail.com>
2026-07-15 13:13:03 +0530

    Fix homepage section order to match the nav sequence
    Amenities and Location were swapped relative to the header nav, so
    clicking through the menu didn't scroll in the order the links implied.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -68,10 +68,8 @@ export default function Home() {
         <HeroSection onOpenEnquirePopup={openEnquirePopup} />
         <ProjectOverviewSection onOpenEnquirePopup={openEnquirePopup} />
         <FloorPlanSection onOpenEnquirePopup={openEnquirePopup} />
-
-        <AmenitiesSection />
-
         <LocationTabsSection onOpenEnquirePopup={openEnquirePopup} />
+        <AmenitiesSection />
         <GallerySection />
         <AboutSection />
 
